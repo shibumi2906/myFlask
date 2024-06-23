8ee008b53f98514a7073a130e3c0fc89ebac4e9e
shibumi2906 <164049385+shibumi2906@users.noreply.github.com>
2024-06-23 17:14:57 +0300

    шаблон

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/blog')
 def blog():
@@ -16,3 +20,4 @@ def contacts():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
